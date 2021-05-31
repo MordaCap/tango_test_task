276d49a785714c24ad3aa6415e71e161493dd018
mordacap <wl.sh.pavlov@gmail.com>
2021-05-31 21:19:53 +0300

    cache level described in get_test method

--- main.py
+++ main.py
@@ -24,10 +24,10 @@ def cache(func):
 
 def get_cache():
     if ttl_cache.get('uid'):
-        return ttl_cache.get('uid')
+        return f"ttl_cache: ttl_cache.get('uid')"
     else:
         ttl_cache['uid'] = redis_client.get('uid')
-        return redis_client.get('uid')
+        return f"redis_cache: redis_client.get('uid')"
 
 
 @app.route('/')
@@ -48,7 +48,7 @@ def test(uid):
 
 @app.route('/get_test', methods=['GET'])
 def get_test():
-    return f'{get_cache()}'
+    return get_cache()
 
 
 if __name__ == '__main__':
