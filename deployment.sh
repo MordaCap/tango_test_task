65bc500c3f8d6b6e361e17bed852666844435ff3
mordacap <wl.sh.pavlov@gmail.com>
2021-05-31 21:47:34 +0300

    deployment.sh update

--- deployment.sh
+++ deployment.sh
@@ -1,14 +1,15 @@
 #bin/bash
 
-gcloud builds submit --tag gcr.io/regal-state-230718/tango-test-task
+gcloud builds submit --tag gcr.io/regal-state-230718/tango-test
 
-gcloud run deploy \                                                 
---image gcr.io/regal-state-230718/tango-test-task \
---platform managed \
---allow-unauthenticated \
---region us-central1 \
---vpc-connector redis-connect \
---set-env-vars REDISHOST=10.206.9.131,REDISPORT=6378
+gcloud run deploy \
+  --image gcr.io/regal-state-230718/tango-test \
+  --platform managed \
+  --allow-unauthenticated \
+  --region us-central1 \
+  --vpc-connector redis-connect \
+  --set-env-vars REDISHOST=10.108.243.219,REDISPORT=6379
+
 
 
 
